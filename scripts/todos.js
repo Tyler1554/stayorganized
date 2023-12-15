b20b4beef1f6a290b01115e593c7faf53adb1c11
Tyler1554 <tyler1554@yahoo.com>
2023-12-15 11:17:46 -0500

    user drop working

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,15 +1,17 @@
-"use strict"
-// const todoDrop = document.querySelector("#todo-drop");
+"use strict";
+const userDrop = document.querySelector("#user-drop");
 
-function loadTodoDrop(){
-    fetch("http://localhost:8083/api/users")
+function loadUserDrop() {
+  fetch("http://localhost:8083/api/users")
     .then((response) => response.json())
     .then((users) => {
-        for (const user of users) {
-            let userName = user.username
-            console.log(userName);
-        }
-    })
+      for (const user of users) {
+        let option = document.createElement("option");
+        option.value = user.id;
+        option.innerText = user.name;
+        userDrop.appendChild(option);
+      }
+    });
 }
 
-loadTodoDrop();
+loadUserDrop();
